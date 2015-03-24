e290053bb223f14edc37901c378a791fa063ff2b
Anders Aspnas <aaspnas@gmail.com>
2015-03-24 18:28:37 +0000

    WMI commands added

--- PSRecorder/sysrec.ps1
+++ PSRecorder/sysrec.ps1
@@ -1,3 +1,6 @@
 ﻿function sysrec {
-
+$a =  gwmi -class Win32_PerfFormattedData_PerfOS_System
+gwmi -class Win32_PerfFormattedData_PerfOS_Memory
+gwmi -class Win32_OperatingSystem
+gwmi -class Win32_PerfFormattedData_Tcpip_NetworkInterface
 }
