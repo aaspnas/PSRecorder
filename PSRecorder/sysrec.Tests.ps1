9b88174540e8e736f64f454533ddc897b3ab5744
Anders Aspnas <aaspnas@gmail.com>
2015-03-26 20:59:37 +0000

    WMI tests and raw output

--- PSRecorder/sysrec.Tests.ps1
+++ PSRecorder/sysrec.Tests.ps1
@@ -13,6 +13,14 @@ Describe "sysrec" {
         test-path "$logdir\testfile.log" | Should Be $true
         del "$logdir\testfile.log" -Force
     }
+    It "can create log with measurement line" {
+        $foo = sysrec
+        test-path "$logdir\sysrec.raw" | Should Be $true
+        $cont = gc "$logdir\sysrec.raw"
+        $cont.count -ge 1 | Should Be $true
+
+    }
+
 }
 Describe "get-timestamp" {
     $ts = get-timestamp -s
@@ -22,11 +30,14 @@ Describe "get-timestamp" {
         $ts -match '^\d+$' | Should Be $true
         $us.Length -ge 16 | Should Be $true
         $us -match '^\d+\.\d+$' | Should Be $true
-
     }
 }
 
 
-Describe "get-epoc" {
+Describe "can obtain gwmi data" {
+    It "can get win32_bios" {
+        $wmidata = gwmi -Class Win32_Bios
+        $wmidata.SerialNumber.Length -ge 20 | Should Be $true
+    }
 
 }
--- PSRecorder/sysrec.ps1
+++ PSRecorder/sysrec.ps1
@@ -22,12 +22,16 @@ function create-logdir () {
 
 function sysrec {
     $stat = create-logdir
+    $logfile = "$logdir\sysrec.raw"
     $time = get-timestamp
+    $proc = gwmi -class Win32_PerfFormattedData_PerfOS_Processor
     $osp =  gwmi -class Win32_PerfFormattedData_PerfOS_System
     $mem = gwmi -class Win32_PerfFormattedData_PerfOS_Memory
     $conf = gwmi -class Win32_OperatingSystem
     $net = gwmi -class Win32_PerfFormattedData_Tcpip_NetworkInterface
 
+    $line = ""
+    $line += $time
 
-
+    $line >> $logfile
 }
