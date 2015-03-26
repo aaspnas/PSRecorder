d2e82cc6ef56b00d9230f7cffbf0ff8a34ee59f2
Anders Aspnas <aaspnas@gmail.com>
2015-03-26 20:24:29 +0000

    TimeStamp implemented with tests, dir creation implemented

--- PSRecorder/sysrec.Tests.ps1
+++ PSRecorder/sysrec.Tests.ps1
@@ -5,10 +5,27 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 $logdir = "C:\kronometrix"
 
 Describe "sysrec" {
-    It "can write files" {
-        test-path "$logdir\sysrec" | Should Be $true
+    It "logdir exists" {
+        test-path "$logdir" | Should Be $true
+    }
+    It "can write files to logdir" {
+        "test" > "$logdir\testfile.log"
+        test-path "$logdir\testfile.log" | Should Be $true
+        del "$logdir\testfile.log" -Force
     }
 }
+Describe "get-timestamp" {
+    $ts = get-timestamp -s
+    $us = get-timestamp 
+    It "can obtain EPOC" {
+        $ts.Length -ge 10 | Should Be $true
+        $ts -match '^\d+$' | Should Be $true
+        $us.Length -ge 16 | Should Be $true
+        $us -match '^\d+\.\d+$' | Should Be $true
+
+    }
+}
+
 
 Describe "get-epoc" {
 
--- PSRecorder/sysrec.ps1
+++ PSRecorder/sysrec.ps1
@@ -1,11 +1,32 @@
 ﻿$logdir = "C:\kronometrix"
 
+function get-timestamp ([switch]$s = $false) {
+    if ($s) {
+        return (Get-Date -UFormat "%s").Split('.')[0]
+    } else {
+        return (Get-Date -UFormat "%s")  
+    }
+
+}
+
+function create-logdir () {
+    if (Test-Path $logdir) {
+        return 2
+    } else {
+        $d = mkdir -Path $logdir
+        return 3
+    }
+}
+
+
 
 function sysrec {
-$osp =  gwmi -class Win32_PerfFormattedData_PerfOS_System
-$mem = gwmi -class Win32_PerfFormattedData_PerfOS_Memory
-$conf = gwmi -class Win32_OperatingSystem
-$net = gwmi -class Win32_PerfFormattedData_Tcpip_NetworkInterface
+    $stat = create-logdir
+    $time = get-timestamp
+    $osp =  gwmi -class Win32_PerfFormattedData_PerfOS_System
+    $mem = gwmi -class Win32_PerfFormattedData_PerfOS_Memory
+    $conf = gwmi -class Win32_OperatingSystem
+    $net = gwmi -class Win32_PerfFormattedData_Tcpip_NetworkInterface
 
 
 
