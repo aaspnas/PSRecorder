0d323ee4078f605beffbbc097f7284968e1a3d1c
Anders Aspnas <aaspnas@gmail.com>
2015-03-27 21:31:01 +0000

    added while loop, help switch and version switch

--- PSRecorder/sysrec.ps1
+++ PSRecorder/sysrec.ps1
@@ -2,18 +2,50 @@
 . $td\common.ps1
 
 
+
 function sysrec {
+
+    Param(
+    [switch]$h,
+    [switch]$v,
+    [int]$delay = 60,
+    [int]$maxiteration = 0
+    )
+
+    $iteration = 0
+    $continue = $true
+    if ($h) {return display-helptxt}
+    if ($v) {return display-versiontxt}
+
     $stat = create-logdir
     $logfile = "$logdir\sysrec.raw"
-    $time = get-timestamp
-    $proc = gwmi -class Win32_PerfFormattedData_PerfOS_Processor
-    $osp =  gwmi -class Win32_PerfFormattedData_PerfOS_System
-    $mem = gwmi -class Win32_PerfFormattedData_PerfOS_Memory
-    $conf = gwmi -class Win32_OperatingSystem
-    $net = gwmi -class Win32_PerfFormattedData_Tcpip_NetworkInterface
+    while ($continue) {
+        if ($maxiteration) {
+            $iteration++
+            if ($iteration -ge $maxiteration) {
+                $continue = $false
+            }
+        }
 
-    $line = ""
-    $line += $time
+        $starttime = get-timestamp
+        $proc = gwmi -class Win32_PerfFormattedData_PerfOS_Processor
+        $osp =  gwmi -class Win32_PerfFormattedData_PerfOS_System
+        $mem = gwmi -class Win32_PerfFormattedData_PerfOS_Memory
+        $conf = gwmi -class Win32_OperatingSystem
+        $net = gwmi -class Win32_PerfFormattedData_Tcpip_NetworkInterface
 
-    $line >> $logfile
+        $line = ""
+        $line += $stattime
+
+        $line >> $logfile
+        $endtime = get-timestamp
+        $sleeptime = $delay - ($endtime - $starttime)
+        if ($sleeptime -gt 0) {
+            Start-Sleep -Seconds $sleeptime
+        }
+
+    }
 }
+
+function display-helptxt {}
+function display-versiontxt {}
