66330d3fd84655ce025fb8c84b776a0c9c011401
Anders Aspnas <aaspnas@gmail.com>
2015-04-01 20:13:55 +0000

    Implementation added, tests added, working version

--- PSRecorder/hdwrec.Tests.ps1
+++ PSRecorder/hdwrec.Tests.ps1
@@ -6,4 +6,14 @@ Describe "hdwrec" {
     It "Displays help" {
         (hdwrec -h)  | Should Be $true
     }
+    It "Displays version" {
+        (hdwrec -h)  | Should Be $true
+    }
+    It "Displays data" {
+        (hdwrec 1 1).Split(';').Count -gt 9  | Should Be $true
+    }
+    It "iterates ten times" {
+        (hdwrec 0 10).Split(';').Count -gt 9  | Should Be $true
+    }    
+
 }
--- PSRecorder/hdwrec.ps1
+++ PSRecorder/hdwrec.ps1
@@ -3,4 +3,62 @@
 
 function hdwrec {
 
+    Param(
+    [switch]$h,
+    [switch]$v,
+    [int]$interval = 60,
+    [int]$count = 0
+    )
+
+    $iteration = 0
+    $continue = $true
+    if ($h) {return display-helptxt}
+    if ($v) {return display-versiontxt}
+
+    $stat = create-logdir
+    $logfile = "$logdir\hdwrec.raw"
+    while ($continue) {
+        if ($count) {
+            $iteration++
+            if ($iteration -ge $count) {
+                $continue = $false
+            }
+        }
+
+        $starttime = get-timestamp
+        $wos = gwmi -class Win32_OperatingSystem
+        $wcs = gwmi -class Win32_ComputerSystem
+        $wproc = gwmi -class Win32_Processor
+        $hdw = $wproc.AddressWidth
+        ($maj, $min, $build) = $wos.Version.Split('.')
+        $relos = $maj + '.' + $min  # Note: [environment]::OSVersion is deprecated
+        $kernel = $build
+        $pcpu = $wcs.NumberOfProcessors
+        $vcpu = $wcs.NumberOfLogicalProcessors
+        $memtotal = $wos.TotalVisibleMemorySize
+        $swaptotal = $wos.SizeStoredInPagingFiles
+        $freemem = $wos.FreePhysicalMemory
+
+#   $cult = New-Object System.Globalization.CultureInfo("en-US")
+#        $line = [string]::Format("{0:N0};{1};", 1, "foo");
+#        $line = ""
+
+        $line = [string]"" + $starttime 
+        $line += ";" + (hostname) + ';' + $hdw
+        $line += ";" + $relos + ';' + $kernel
+        $line += ";" + $pcpu + ';' + $vcpu
+        $line += ";" + $memtotal + ';' + $swaptotal + ';' + $freemem
+        $line # >> $logfile
+        $endtime = get-timestamp
+        $sleeptime = $interval - ($endtime - $starttime)
+        if (($sleeptime -gt 0) -and ($continue)) {
+            Start-Sleep -Seconds $sleeptime
+        }
+
+    }
+
 }
+
+function display-helptxt {"Not implemented"}
+function display-versiontxt {"1.0"}
+
