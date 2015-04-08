59bd64d0376a7be05f6b396e081338c7a73c879a
Anders Aspnas <aaspnas@gmail.com>
2015-04-08 19:34:27 +0000

    Installation of hdwrec service using NSSM

---
+++ ServiceInstall/nssmInstHdwRec.ps1
@@ -0,0 +1,12 @@
+﻿$cmdline = "-ExecutionPolicy remotesigned -File hdwrec.ps1"
+$appPS = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+$AppStderr = "C:\kronometrix\stderr.log"
+$AppStdout = "C:\kronometrix\stdout.log"
+$AppDirectory = "C:\Users\aaspnas\scripts\PSRecorder"
+
+nssm install hdwrec $appPS $cmdline
+nssm set hdwrec AppDirectory $AppDirectory 
+nssm set hdwrec Application $appPS
+nssm set hdwrec AppParameters $cmdline
+nssm set hdwrec AppStdout $AppStdout
+nssm set hdwrec AppStderr $AppStderr
