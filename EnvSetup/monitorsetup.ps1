46102d538e2079c70c212769ff7dcdfef23bfb97
Anders Aspnas <aaspnas@gmail.com>
2015-04-08 20:10:35 +0000

    Installation script for monitoring scripts only installation, first version

---
+++ EnvSetup/monitorsetup.ps1
@@ -0,0 +1,14 @@
+﻿## Target to have this PowerShell version 2 compatible
+
+## create dir
+$rootdir = [string]$env:HOME + "\scripts"
+if ( Test-Path -Path $rootdir ) { 
+    cd $rootdir
+} else {
+    mkdir $rootdir
+    cd $rootdir
+}
+## copy scripts from github
+
+## Create service
+## Test and verification functions
